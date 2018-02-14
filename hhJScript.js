ed737b2866ae8b2dfbe69b93f036ee681feba21d
Adam Specker <specker@gmail.com>
2018-02-14 11:41:34 -0500

    added initial bar objects to javascript

---
+++ hhJScript.js
@@ -0,0 +1,97 @@
+// JAVASCRIPT FOR HAPPY HOUR PROJECT
+// CHRIS, ADRIAN, DANIEL, ADAM
+
+// Declare restaurant objects
+//GEORGETOWN
+var jPauls = {
+    name: "J. Paul’s",
+    location: "georgetown",
+    foodType: "american",
+    price: 2,
+    cocktail: true,
+    beer: true,
+    wine: true
+}
+var pizzeriaParadiso = {
+	name: "Pizzeria Paradiso",
+	location:"georgetown",
+	foodType: "italian",
+	price: 2,
+	cocktail = false,
+	beer = true,
+    wine = true
+}	
+var oldGlory = {
+	name: "Old Glory BBQ",
+	location:"georgetown",
+	foodType: "southern",
+	price: 2,
+	cocktail = true,
+	beer = true,
+    wine = true
+}	
+var sovereign = {
+	name: "The Sovereign",
+	location:"georgetown",
+	foodType: "european",
+	price: 2,
+	cocktail = true,
+	beer = true,
+    wine = true
+}	
+var theTombs = {
+	name: "The Tombs",
+	location:"georgetown",
+	foodType: "american",
+	price: 1,
+	cocktail = false,
+	beer = true,
+    wine = true
+}	
+// FRIENDSHIP-TENLEYTOWN
+var chefGeoffs = {
+	name: "Chef Geoff's",
+	location:"friendship",
+	foodType: "american",
+	price: 2,
+	cocktail = true,
+	beer = true,
+    wine = true
+}	
+var chatter = {
+	name: "Chatter",
+	location:"friendship",
+	foodType: "american",
+	price: 2,
+	cocktail = true,
+	beer = true,
+    wine = true
+}	
+var casaLuca = {
+	name: "Casa Luca",
+	location:"friendship",
+	foodType: "italian",
+	price: 2,
+	cocktail = true,
+	beer = true,
+    wine = true
+}	
+var frontPage = {
+	name: "Front Page DC",
+	location:"friendship",
+	foodType: "american",
+	price: 2,
+	cocktail = true,
+	beer = true,
+    wine = true
+}	
+var yosaku = {
+	name: "Yosaku Japanese",
+	location:"friendship",
+	foodType: "asian",
+	price: 2,
+	cocktail = true,
+	beer = true,
+    wine = true
+}	
+// ADAM'S MORGAN
