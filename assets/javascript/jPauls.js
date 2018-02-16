0acc276d37ca5a7251d7b67d8688135ec3167831
unknown <crawford7100@gmail.com>
2018-02-16 11:06:50 -0500

    linked test leaflet

---
+++ assets/javascript/jPauls.js
@@ -0,0 +1,39 @@
+//Restaurant Name//
+$('#restaurantName').text("J. Paul's")
+
+//Happy Hour Information//
+$('#happyHourInfo').text("$5 Cocktails; $3 Beer; $1 Blue Points Oysters; .50 Wings; $6 Tuna Roll")
+
+//Food Selections//
+$('#food1').text("Margherita Bruschetta Bites")
+$('#food2').text("Sun-Dried Tomato and Herb Hummus")
+$('#food3').text("Southwest Nachos")
+
+//The Recommendations//
+$('#chris').text("J.paul's Amber Ale")
+$('#adrian').text("Leffe Blonde Ale")
+$('#daniel').text("Bold Rock Virginia Apple")
+$('#adam').text("Brooklyn Brown Ale")
+
+//Restaurant Info//
+$('#restaurantInfo').text("J. Paul's")
+$('#address1').text("3218 M St NW")
+$('#address2').text("Washington, DC 20007")
+$('#address3').text("")
+$('#phone').text("202.333.3450")
+$('#website').text("http://jpaulsdc.com/")
+
+//Restuarant Hours//
+$('#happyHours').text("Monday-Friday 4p-7p")
+$('#hours1').text("Mon-Wed: 11:30a-Midnight")
+$('#hours2').text("Thu: 11:30a-2a")
+$('#hours3').text("Fri: 11:30a-2a")
+$('#hours4').text("Sat: 10:30a-2a")
+$('#hours5').text("Sun: 10:30-Midnight")
+
+//Map Info//
+
+$('#gps1').text("38.905006,-77.063467")
+$('#gps2').text("38.905006,-77.063467")
+
+38.905006,-77.063467
